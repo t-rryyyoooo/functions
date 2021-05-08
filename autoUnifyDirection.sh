15108a48c545ef2751bd536b00e4b59bd3793d68
Tanimoto Ryo <rryyyoooo@i.softbank.jp>
2021-05-08 14:58:08 +0900

    R

---
+++ autoUnifyDirection.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+# Input 
+readonly INPUT_DIRECTORY="input"
+echo -n "Is json file name unifyDirection.json?[y/n]:"
+read which
+while [ ! $which = "y" -a ! $which = "n" ]
+do
+ echo -n "Is json file name the same as this file name?[y/n]:"
+ read which
+done
+
+# Specify json file.
+if [ $which = "y" ];then
+ JSON_NAME="unifyDirection.json"
+else
+ echo -n "JSON_FILE_NAME="
+ read JSON_NAME
+fi
+
+# From json file, read required variables.
+readonly JSON_FILE="${INPUT_DIRECTORY}/${JSON_NAME}"
+readonly DATA_DIRECTORY=$(eval echo $(cat ${JSON_FILE} | jq -r ".data_directory"))
+readonly SAVE_DIRECTORY=$(eval echo $(cat ${JSON_FILE} | jq -r ".save_directory"))
+readonly REF_PATH=$(eval echo $(cat ${JSON_FILE} | jq -r ".ref_path"))
+readonly INPUT_NAME=$(cat ${JSON_FILE} | jq -r ".input_name")
+readonly SAVE_NAME=$(cat ${JSON_FILE} | jq -r ".save_name")
+
+readonly NUM_ARRAY=$(cat ${JSON_FILE} | jq -r ".num_array[]")
+readonly LOG_FILE=$(eval echo $(cat ${JSON_FILE} | jq -r ".log_file"))
+
+echo "LOG_FILE:${LOG_FILE}"
+
+# Make directory to save LOG.
+mkdir -p `dirname ${LOG_FILE}`
+date >> $LOG_FILE
+
+for number in ${NUM_ARRAY[@]}
+do
+ image="${DATA_DIRECTORY}/case_${number}/${INPUT_NAME}"
+ save="${SAVE_DIRECTORY}/case_${number}/${SAVE_NAME}"
+
+ echo "image:${image}"
+ echo "REF_PATH:${REF_PATH}"
+ echo "save:${save}"
+
+ python3 unifyDirection.py ${image} ${REF_PATH} ${save}
+
+ # Judge if it works.
+ if [ $? -eq 0 ]; then
+  echo "case_${number} done."
+ 
+ else
+  echo "case_${number}" >> $LOG_FILE
+  echo "case_${number} failed"
+ 
+ fi
+
+done
+
+
---
+++ unifyDirection.py
@@ -0,0 +1,33 @@
+import argparse
+import SimpleITK as sitk
+import numpy as np
+import sys
+sys.path.append("..")
+from utils.utils import getImageWithMeta
+from pathlib import Path
+
+def parseArgs():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("image_path")
+    parser.add_argument("ref_path")
+    parser.add_argument("save_path")
+
+    args = parser.parse_args()
+
+    return args
+
+def main(args):
+    image = sitk.ReadImage(args.image_path)
+    ref   = sitk.ReadImage(args.ref_path)
+
+    image_array = sitk.GetArrayFromImage(image)
+
+    unified_image = getImageWithMeta(image_array[::-1, ::-1, :], ref)
+    Path(args.save_path).parent.mkdir(exist_ok=True)
+
+    sitk.WriteImage(unified_image, args.save_path)
+
+if __name__ == "__main__":
+    args = parseArgs()
+    main(args)
